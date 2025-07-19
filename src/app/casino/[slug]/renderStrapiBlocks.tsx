8f3133d4da99a685756a1a3ca8c09db02180992c
Nickz-dev <nick.apios@gmail.com>
2025-07-19 18:38:01 +0700

    first commit

--- src/app/casino/[slug]/renderStrapiBlocks.tsx
+++ src/app/casino/[slug]/renderStrapiBlocks.tsx
@@ -6,6 +6,7 @@ function renderRichText(nodes: any[]) {
   return nodes.map((node, idx) => {
     // Ссылка
     if (node.type === "link" && node.url) {
+      console.log("LINK NODE:", node);
       if (node.url.startsWith("#")) {
         // Якорь внутри страницы
         const children = node.children ? node.children.map(renderRichText) : node.text;
